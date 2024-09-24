de3217d69d0aafb1abbfd39ac2e24acf31ea27a4
TrickyVikram <vkumarsah999@gmail.com>
2024-09-24 23:52:19 +0530

    nav1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import EcommerceNavbar from './components/Navbar';
 
 // import './App.css';
-import Header from './components/Header';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <Header/>
-      </header>
+   <EcommerceNavbar />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 
-function Navbar() {
+function EcommerceNavbar() {
   return (
-    <div>
-      
-    </div>
-  )
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">
+        <img
+          src="path-to-your-logo.png"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+          alt="Brand logo"
+        />{' '}
+        E-Shop
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#shop">Shop</Nav.Link>
+          <Nav.Link href="#cart">Cart</Nav.Link>
+          <NavDropdown title="Account" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#profile">Profile</NavDropdown.Item>
+            <NavDropdown.Item href="#login">Login</NavDropdown.Item>
+            <NavDropdown.Item href="#register">Register</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Form inline className="d-flex">
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
-export default Navbar
+export default EcommerceNavbar;
