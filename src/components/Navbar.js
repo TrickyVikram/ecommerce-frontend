e3c4b0b7718f59747f0c0e63bb8c19a195b8c469
TrickyVikram <vkumarsah999@gmail.com>
2024-09-25 00:37:12 +0530

    nav2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,26 +11,31 @@ function EcommerceNavbar() {
           width="30"
           height="30"
           className="d-inline-block align-top"
-          alt="Brand logo"
+          alt=""
         />{' '}
         E-Shop
       </Navbar.Brand>
+
+      <Form className="d-flex">
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button style={{ marginLeft: "-81px" }} className='s-btn' variant="outline-success">
+          Search
+        </Button>
+      </Form>
+
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#shop">Shop</Nav.Link>
           <Nav.Link href="#cart">Cart</Nav.Link>
-          <NavDropdown title="Account" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#profile">Profile</NavDropdown.Item>
-            <NavDropdown.Item href="#login">Login</NavDropdown.Item>
-            <NavDropdown.Item href="#register">Register</NavDropdown.Item>
-          </NavDropdown>
         </Nav>
-        <Form className="d-flex">
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form>
+
+        <NavDropdown title="Account" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#profile">Profile</NavDropdown.Item>
+          <NavDropdown.Item href="#login">Login</NavDropdown.Item>
+          <NavDropdown.Item href="#register">Register</NavDropdown.Item>
+          <NavDropdown.Item href="#logout">Logout</NavDropdown.Item>
+        </NavDropdown>
       </Navbar.Collapse>
     </Navbar>
   );
